302e30c5b21dc485fc69d513e20e9548949e1697
Venkatappachari Bangaru <venkatappachari.bangaru@int.whishworks.com>
2019-10-17 16:52:44 +0530

    uncommenting extra test case

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -8,20 +8,20 @@ import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
-	/*@Test
+	@Test
 	public void mavenFlowReturnsHelloMaven() throws Exception {
 		runFlowAndExpect("mavenFlow", "Hello Maven");
-	}*/
+	}
 
 	@Override
 	protected String getConfigFile() {
 		return "maven-project-venkat-jms.xml";
 	}
 
-	/*@Test
+	@Test
 	public void retrieveFlightsAddsAppropriateHeader() throws Exception {
 		MuleEvent event = runFlow("retrieveFlights");
 		String contentType = event.getMessage().getOutboundProperty("Content-Type");
 		assertEquals("application/json", contentType);
-	}*/
+	}
 }
