3648f371b46b9e25d7d169ce8cff3dd4c89edb11
Venkatappachari Bangaru <venkatappachari.bangaru@int.whishworks.com>
2019-10-17 16:25:22 +0530

    adding functional data

---
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -0,0 +1,27 @@
+package com.mulesoft.training;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.mule.api.MuleEvent;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+public class HelloMavenTest extends FunctionalTestCase {
+
+	@Test
+	public void mavenFlowReturnsHelloMaven() throws Exception {
+		runFlowAndExpect("mavenFlow", "Hello Maven");
+	}
+
+	@Override
+	protected String getConfigFile() {
+		return "maven-project-venkat-jms.xml";
+	}
+
+	@Test
+	public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+		MuleEvent event = runFlow("retrieveFlights");
+		String contentType = event.getMessage().getOutboundProperty("Content-Type");
+		assertEquals("application/json", contentType);
+	}
+}
