7a8862ab8f6f16e31ed6d0b3ce9fe0a983afeaac
Venkatappachari Bangaru <venkatappachari.bangaru@int.whishworks.com>
2019-10-17 16:37:26 +0530

    commenting extra test case

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -8,10 +8,10 @@ import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
-	@Test
+	/*@Test
 	public void mavenFlowReturnsHelloMaven() throws Exception {
 		runFlowAndExpect("mavenFlow", "Hello Maven");
-	}
+	}*/
 
 	@Override
 	protected String getConfigFile() {
