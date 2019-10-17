d3ee5a98281b41b0e687a3e9efbc47d01a431434
Venkatappachari Bangaru <venkatappachari.bangaru@int.whishworks.com>
2019-10-17 16:35:25 +0530

    commenting extra test case

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -18,10 +18,10 @@ public class HelloMavenTest extends FunctionalTestCase {
 		return "maven-project-venkat-jms.xml";
 	}
 
-	@Test
+	/*@Test
 	public void retrieveFlightsAddsAppropriateHeader() throws Exception {
 		MuleEvent event = runFlow("retrieveFlights");
 		String contentType = event.getMessage().getOutboundProperty("Content-Type");
 		assertEquals("application/json", contentType);
-	}
+	}*/
 }
